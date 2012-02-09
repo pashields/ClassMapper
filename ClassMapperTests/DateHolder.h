1e5a16535e7e5eec362f835e5327e04a7c6e416f
Pat Shields <yeoldefortran@gmail.com>
2012-02-08 22:29:44 -0500

    Update org file with singleton note.

---
+++ ClassMapperTests/DateHolder.h
@@ -0,0 +1,13 @@
+//
+//  DateHolder.h
+//  ClassMapper
+//
+//  Created by Patrick Shields on 2/8/12.
+//  Copyright (c) 2012 Patrick Shields. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DateHolder : NSObject
+
+@end
---
+++ ClassMapperTests/DateHolder.m
@@ -0,0 +1,13 @@
+//
+//  DateHolder.m
+//  ClassMapper
+//
+//  Created by Patrick Shields on 2/8/12.
+//  Copyright (c) 2012 Patrick Shields. All rights reserved.
+//
+
+#import "DateHolder.h"
+
+@implementation DateHolder
+
+@end
